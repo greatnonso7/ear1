c24d2e0e6a29dab45bb0da10c0d31177e85fd7a4
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-02-17 01:46:29 +0100

    WIP Login screen

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.jsx', '.js', '.json'],
+        alias: {
+          screens: './src/screens',
+          navigation: './src/navigation',
+          components: './src/components',
+          constants: './src/constants',
+          images: './src/assets/images',
+          theme: './src/theme',
+        },
+      },
+    ],
+  ],
 };
